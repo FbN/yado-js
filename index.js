06e5f1b6e59327629e09a010f6a30fd0a235dbb6
Fabiano Taioli <ftaioli@gmail.com>
2021-01-05 00:42:39 +0100

    ok

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-// ArrayDo([
-//     { a: [1, 2] },
-//     { b: [2, 3] },
-//     { return: s => s.a + s + b }
-// ])
-//     s => (s.a > 1 ? { c: [2, 3] } : { return: s.a + s.b }),
-
 const RESULT = Symbol()
 
 const isFunction = statement => typeof statement === 'function'
 
-const isReturn = statement => !!statement?.return
+const isReturn = statement => statement?.return !== undefined
+
+const resultOrChain = chain => scope =>
+    scope[RESULT] !== undefined ? scope[RESULT] : chain(scope)
 
 const Do = ({ pure, bind }) => statemens =>
     [...statemens].reverse().reduce(
         function reducer(chain, statement) {
             if (isFunction(statement)) {
-                return scope =>
-                    scope[RESULT] !== undefined
-                        ? scope[RESULT]
-                        : reducer(chain, statement(scope))(scope)
+                return resultOrChain(scope =>
+                    reducer(chain, statement(scope))(scope)
+                )
             }
             if (isReturn(statement)) {
-                return scope =>
-                    scope[RESULT] !== undefined
-                        ? scope[RESULT]
-                        : chain({
-                              ...scope,
-                              [RESULT]: pure(statement.return(scope))
-                          })
+                return resultOrChain(scope =>
+                    chain({
+                        ...scope,
+                        [RESULT]: pure(
+                            isFunction(statement.return)
+                                ? statement.return(scope)
+                                : statement.return
+                        )
+                    })
+                )
             }
             return Object.entries(statement)
                 .reverse()
                 .reduce(
-                    (chain, [identifier, argument]) => scope =>
-                        scope[RESULT] !== undefined
-                            ? scope[RESULT]
-                            : bind(argument)(v =>
-                                  chain({ ...scope, [identifier]: v })
-                              ),
+                    (chain, [identifier, argument]) =>
+                        resultOrChain(scope =>
+                            bind(argument)(v =>
+                                chain({ ...scope, [identifier]: v })
+                            )
+                        ),
                     chain
                 )
         },
